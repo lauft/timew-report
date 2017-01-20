5a50edbfe5d743d813b972445b7afef4c1035092
Thomas Lauf <thomas.lauf@tngtech.com>
2017-01-20 21:43:10 +0100

    Add methods to get and update configuration

--- timewreport/config.py
+++ timewreport/config.py
@@ -5,6 +5,19 @@ class TimeWarriorConfig(object):
     def __init__(self, config=None):
         self.__config = config if config is not None else {}
 
+    def update(self, other):
+        if isinstance(other, TimeWarriorConfig):
+            config = other.get_dict()
+        elif isinstance(other, dict):
+            config = other
+        else:
+            raise TypeError()
+
+        self.__config.update(config)
+
+    def get_dict(self):
+        return self.__config
+
     def get_value(self, key, default):
         if key in self.__config:
             return self.__config[key]
