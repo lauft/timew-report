66a7e939fece2e7ddeff7b0ac858459e93a6a3d6
Thomas Lauf <thomas.lauf@tngtech.com>
2020-05-22 17:22:53 +0200

    Replace function call with set literal
    - Add assert statement so we do not have a "statement with no effect"

--- tests/interval_test.py
+++ tests/interval_test.py
@@ -5,4 +5,4 @@ def test_interval_should_be_hashable():
     a = TimeWarriorInterval("20180816T100209Z", "20180816T110209Z", [], None)
     b = TimeWarriorInterval("20180816T090319Z", "20180816T100700Z", [], None)
 
-    set([a, b])
+    assert {a, b}
