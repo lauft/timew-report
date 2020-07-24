f8d6cf4c60228a84217e1e5de640c79d8cae0973
Maximilian Imgrund <maximilian.imgrund@tngtech.com>
2020-07-24 08:31:46 +0200

    Add possible annotations to parsed list of intervals
    Signed-off-by: Maximilian Imgrund <maximilian.imgrund@tngtech.com>

--- timewreport/interval.py
+++ timewreport/interval.py
@@ -5,15 +5,17 @@ from dateutil.tz import tz
 
 
 class TimeWarriorInterval(object):
-    def __init__(self, start, end, tags):
+    def __init__(self, start, end, tags, annotation):
         self.__start = self.__get_local_datetime(start)
         self.__end = self.__get_local_datetime(end) if end is not None else None
         self.__tags = tags
+        self.__annotation = annotation
 
     def __eq__(self, other):
         return self.__start == other.get_start() \
             and self.__end == other.get_end() \
-            and self.__tags == other.get_tags()
+            and self.__tags == other.get_tags()\
+            and self.__annotation == other.get_annotation()
 
     def __hash__(self):
         return hash(repr(self))
@@ -38,6 +40,9 @@ class TimeWarriorInterval(object):
 
     def get_date(self):
         return date(self.__start.year, self.__start.month, self.__start.day)
+
+    def get_annotation(self):
+        return self.__annotation
 
     def __get_local_datetime(self, datetime_string):
         from_zone = tz.tzutc()
