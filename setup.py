c972357a44640c7a5f803d79fc77ca597e1b22f0
Thomas Lauf <thomas.lauf@tngtech.com>
2020-08-01 22:29:29 +0200

    Drop support for python 2.7

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ config = {
         'Intended Audience :: Developers',
         'Topic :: Utilities',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
     ],
     'keywords': 'timewarrior taskwarrior time-tracking',
