162e96cf426d2867764f99f7f5ef6af118ce2b19
Thomas Lauf <thomas.lauf@tngtech.com>
2020-08-01 22:23:13 +0200

    Small fixups: Move function up, add space

--- timewreport/interval.py
+++ timewreport/interval.py
@@ -14,7 +14,7 @@ class TimeWarriorInterval(object):
     def __eq__(self, other):
         return self.__start == other.get_start() \
             and self.__end == other.get_end() \
-            and self.__tags == other.get_tags()\
+            and self.__tags == other.get_tags() \
             and self.__annotation == other.get_annotation()
 
     def __hash__(self):
@@ -29,6 +29,9 @@ class TimeWarriorInterval(object):
     def get_tags(self):
         return self.__tags
 
+    def get_annotation(self):
+        return self.__annotation
+
     def is_open(self):
         return self.__end is None
 
@@ -40,9 +43,6 @@ class TimeWarriorInterval(object):
 
     def get_date(self):
         return date(self.__start.year, self.__start.month, self.__start.day)
-
-    def get_annotation(self):
-        return self.__annotation
 
     def __get_local_datetime(self, datetime_string):
         from_zone = tz.tzutc()
