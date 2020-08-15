724ccc578c6272c2842e7b4399f92f653cd932af
Thomas Lauf <thomas.lauf@tngtech.com>
2020-08-15 22:43:24 +0200

    Add tests for `config.get_int`

--- tests/config_test.py
+++ tests/config_test.py
@@ -41,3 +41,22 @@ def test_get_boolean_should_return_default_if_key_not_available():
     config = TimeWarriorConfig({})
 
     assert config.get_boolean('FOO', True) is True
+
+
+def test_get_int_should_return_value_for_valid_strings():
+    config = TimeWarriorConfig({'KEY': '1'})
+
+    assert config.get_int('KEY', 2) == 1
+
+
+def test_get_int_should_return_raise_exception_for_invalid_strings():
+    with pytest.raises(ValueError):
+        config = TimeWarriorConfig({'KEY': 'bla'})
+
+        config.get_int('KEY', 2)
+
+
+def test_get_int_should_return_default_if_key_not_available():
+    config = TimeWarriorConfig({'BAR': '1'})
+
+    assert config.get_int('FOO', 2) is 2
