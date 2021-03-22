860b88669dca0acf839ef2074f1f1be9e03ff639
Thomas Lauf <thomas.lauf@tngtech.com>
2021-03-22 21:47:33 +0100

    Add tests for interval

--- tests/interval_test.py
+++ tests/interval_test.py
@@ -1,3 +1,7 @@
+from datetime import datetime, timedelta
+
+from dateutil.tz import tz
+
 from timewreport.interval import TimeWarriorInterval
 
 
@@ -6,3 +10,31 @@ def test_interval_should_be_hashable():
     b = TimeWarriorInterval("20180816T090319Z", "20180816T100700Z", [], None)
 
     assert {a, b}
+
+
+def test_interval_should_be_creatable_from_utc_string():
+    test_start = datetime.now(tz=tz.tzlocal()).replace(microsecond=0)
+    test_start_utc = test_start.utcnow()
+    test_end = test_start + timedelta(hours=1)
+    test_end_utc = test_start_utc + timedelta(hours=1)
+
+    interval = TimeWarriorInterval(
+        "{:%Y%m%dT%H%M%S}Z".format(test_start_utc),
+        "{:%Y%m%dT%H%M%S}Z".format(test_end_utc),
+        [],
+        None)
+
+    assert interval.get_start() == test_start and interval.get_end() == test_end
+
+
+def test_interval_should_be_creatable_from_local_string():
+    test_start = datetime.now(tz=tz.tzlocal()).replace(microsecond=0)
+    test_end = test_start + timedelta(hours=1)
+
+    interval = TimeWarriorInterval(
+        "{:%Y%m%dT%H%M%S}".format(test_start),
+        "{:%Y%m%dT%H%M%S}".format(test_end),
+        [],
+        None)
+
+    assert interval.get_start() == test_start and interval.get_end() == test_end
